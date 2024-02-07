4b9fe660381fc75835abdd1b7b0eb3edb37b15b5
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-07 13:49:25 +0100

    fix coroutine

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 import random
+from asyncio import sleep
 
-def wait_random(max_delay = 10):
-    yield
-    return random.uniform(0, max_delay)
+async def wait_random(max_delay = 10):
+    delay = random.uniform(0, max_delay)
+    await sleep(delay)
+    return delay
