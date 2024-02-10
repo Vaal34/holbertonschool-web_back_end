b52da72efcd7bc470fbad6bf7d37dd418dd3e4a6
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-10 20:27:03 +0100

    coutine loop 10 times

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
 
 """
-Write an asynchronous coroutine that takes in an integer argument
-(max_delay, with a default value of 10) named wait_random
-that waits for a random delay between 0 and
-max_delay (included and float value) seconds and eventually returns it.
+The coroutine will loop 10 times, each time asynchronously wait 1 second,
+then yield a random number between 0 and 10. Use the random module.
 """
 import random
 import asyncio
