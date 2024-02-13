9e8605c54868ee19d9881937a6afae1f9d2a165e
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-13 11:40:09 +0100

    document the code

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -2,27 +2,24 @@
 """
 Caching
 """
-BaseCaching = __import__('base_caching').BaseCaching
+
+from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """
+    """ 
     Implements a basic cache by extending the BaseCaching class.
 
     This class provides methods to add and retrieve items from the cache.
     """
 
     def put(self, key, item):
-        """
-        add element to dict of cache
-        """
+        """ add element to dict of cache """
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-        get element of dict by key
-        """
+        """ get element of dict by key """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
