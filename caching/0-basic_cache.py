25f57a84d5014eac6f2c0d8a14794f71469c1dd3
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-13 11:35:14 +0100

    document the code

--- caching/0-basic_cache.py
+++ caching/0-basic_cache.py
@@ -13,12 +13,16 @@ class BasicCache(BaseCaching):
     """
 
     def put(self, key, item):
-        """ add element to dict of cache """
+        """ 
+        add element to dict of cache 
+        """
         if key is not None or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """ get element of dict by key """
+        """ 
+        get element of dict by key 
+        """
         if key is None or key not in self.cache_data:
             return None
         return self.cache_data[key]
