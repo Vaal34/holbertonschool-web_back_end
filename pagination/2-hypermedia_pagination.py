17e9e5fbc973f2c7ed3584b358923e03c313c4f4
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-16 12:19:43 +0100

    change page_size

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -34,6 +34,10 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ get data a this page"""
+        assert type(page) is int
+        assert type(page_size) is int
+        assert page_size > 0
+        assert page > 0
 
         first_last_index = index_range(page, page_size)
         first_index = first_last_index[0]
@@ -49,27 +53,20 @@ class Server:
     def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
         """ get data a this page """
         assert type(page) is int
-        page > 0
         assert type(page_size) is int
-        page_size > 0
+        assert page_size > 0
+        assert page > 0
 
-        data = self.get_page(page=page, page_size=page_size)
+        data = self.get_page(page, page_size)
 
         total_page = ceil(len(self.dataset()) / page_size)
 
-        if total_page < page:
-            next_page = None
-        else:
-            next_page = page + 1 if page > 0 else None
-
-        previous_page = page - 1 if page > 1 else None
-
         DictPages = {
-            "page_size": page_size,
+            "page_size": len(data),
             "page": page,
             "data": data,
-            "next_page": next_page,
-            "previous_page": previous_page,
+            "next_page": page + 1 if page < 1 else None,
+            "previous_page": page - 1 if page > 1 else None,
             "total_page": total_page
         }
 
