ab625a53fe1d2287a1d770c15b5fa7f8340183da
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-16 11:55:48 +0100

    test assert

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -47,8 +47,10 @@ class Server:
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
         """ get data a this page """
-        assert isinstance(page, int) and page > 0
-        assert isinstance(page_size, int) and page_size > 0
+        assert isinstance(page, int)
+        page > 0
+        assert isinstance(page_size, int)
+        page_size > 0
 
         data = self.get_page(page=page, page_size=page_size)
 
