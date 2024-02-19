371cd0be1b1b79498cc25b8ab7280e727c574009
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-19 14:08:42 +0100

    implement filter datum in logging.fomat

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 """
 import re
 import typing
+import logging
 
 
 def filter_datum(fields: typing.List[str],
@@ -20,3 +21,20 @@ def filter_datum(fields: typing.List[str],
         message = re.sub(rf"{field}=(.+?){separator}",
                          f"{field}={redaction}{separator}", message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: typing.List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        msg = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, msg, self.SEPARATOR)
