c2dd3c2f538135f62f772d33cb5ea4267cf889bc
Vaal34 <113857350+Vaal34@users.noreply.github.com>
2024-02-19 12:53:11 +0100

    use rgex

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,10 @@ import re
 import typing
 
 
-def filter_datum(fields: typing.List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: typing.List[str],
+                 redaction: str,
+                 message: str,
+                 separator: str) -> str:
     """
     fields: a list of strings representing all fields to obfuscate
     redaction: a string representing by what the field will be obfuscated
@@ -13,9 +16,7 @@ def filter_datum(fields: typing.List[str], redaction: str, message: str, separat
     separator: a string representing by which character is separating
     all fields in the log line (message)
     """
-    textSplit = message.split(separator)
     for field in fields:
-        for info in textSplit:  # info is <info>=<infoValue> example name=bob
-            if re.search(field, info):
-                message = re.sub(info.split("=")[1], redaction, message)
+        message = re.sub(rf"{field}=([^;]+){separator}",
+                         f"{field}={redaction}{separator}", message)
     return message
