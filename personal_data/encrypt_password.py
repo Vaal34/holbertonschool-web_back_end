d403617892df7f2a1dbe1d6ab0e3c02e2b1f66ca
Vaal34 <5619@holbertonstudents.com>
2024-02-20 13:41:33 +0100

    pycode

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -7,6 +7,7 @@ def hash_password(password: str) -> bytes:
     """ hash password """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
 
+
 def is_valid(hashed_password: bytes, password: str) -> bool:
     """ check is password and hashed_password are the same """
     return bcrypt.checkpw(password.encode("UTF-8"), hashed_password)
