9f1c63e4a70d717462ddd1d342f8c903c50e5f20
Vaal34 <5619@holbertonstudents.com>
2024-02-20 13:32:24 +0100

    annontate

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -3,6 +3,6 @@
 import bcrypt
 
 
-def hash_password(password: str) -> str:
+def hash_password(password: str) -> bytes:
     """ hash password """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
