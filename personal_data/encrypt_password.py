402653ac08c8cd176a043c9dc9911b26a75e4364
Vaal34 <5619@holbertonstudents.com>
2024-02-20 13:40:51 +0100

    checking password

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -6,3 +6,7 @@ import bcrypt
 def hash_password(password: str) -> bytes:
     """ hash password """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """ check is password and hashed_password are the same """
+    return bcrypt.checkpw(password.encode("UTF-8"), hashed_password)
