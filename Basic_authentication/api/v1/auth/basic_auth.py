dc6e68cfe8fbcddffa58602a409af1511e873de6
Vaal34 <5619@holbertonstudents.com>
2024-02-21 14:20:43 +0100

    returns the useremail and pswd from the Base64 decoded value.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ Manager API Authentication """
 from flask import request
-from typing import List, TypeVar
+from typing import List, TypeVar, Tuple
 from api.v1.auth.auth import Auth
 from base64 import b64decode
 
@@ -33,3 +33,19 @@ class BasicAuth(Auth):
             return b64decode(base64_authorization_header).decode('UTF-8')
         except Exception:
             return None
+
+    def extract_user_credentials(self,
+                                 decoded_base64_authorization_header: str
+                                 ) -> Tuple(str, str):
+        """  returns the useremail and pswd from the Base64 decoded value. """
+        if decoded_base64_authorization_header is None:
+            return None, None
+        if type(decoded_base64_authorization_header) is not str:
+            return None, None
+        if ":" not in decoded_base64_authorization_header:
+            return None, None
+        else:
+            decoded_base64_authorization_header.split(":")
+            user_email = decoded_base64_authorization_header[0]
+            password = decoded_base64_authorization_header[1]
+            return user_email, password
