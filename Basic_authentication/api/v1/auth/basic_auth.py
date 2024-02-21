ffea0f29aab4d58270021537f1bf5dee00fb6350
Vaal34 <5619@holbertonstudents.com>
2024-02-21 13:35:27 +0100

    Create a class BasicAuth that inherits from Auth

---
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" Manager API Authentication """
+from flask import request
+from typing import List, TypeVar
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """ class BasicAuth inerith from Auth """
+    pass
