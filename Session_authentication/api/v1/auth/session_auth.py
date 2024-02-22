18cdc857dfff6ca522a19451177bccb89bc9d462
Vaal34 <5619@holbertonstudents.com>
2024-02-22 15:07:46 +0100

    create session id with function

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -6,8 +6,22 @@ from api.v1.auth.auth import Auth
 from base64 import b64decode
 from models.user import User
 import json
+import uuid
 
 
 class SessionAuth(Auth):
     """ class SessionUAuth inerith from Auth """
-    pass
+
+    def __init__(self) -> None:
+        """ init """
+        self.user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """ creates a Session ID for a user_id """
+        if user_id is None:
+            return None
+        if type(user_id) is not str:
+            return None
+        session_id = uuid.uuid4()
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
