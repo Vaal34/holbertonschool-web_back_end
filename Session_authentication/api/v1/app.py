af7a054a0b0171686e59015b721f4db6975e19d3
Vaal34 <5619@holbertonstudents.com>
2024-02-22 17:14:38 +0100

    Update the @app.before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -62,10 +62,13 @@ def before_request():
     if auth.require_auth(request.path, exclude_path) is not True:
         return
 
+    if auth.session_cookie(request) is None:
+        return abort(401)
+
     if auth.authorization_header(request) is None \
             and auth.session_cookie(request) is None:
-        abort(401)
-        
+        return abort(401)
+
     if auth.authorization_header(request) is None:
         return abort(401)
 
