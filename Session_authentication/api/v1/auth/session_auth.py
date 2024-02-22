7a1d87eeeeafe2f01acaa3836052cb7b3db0b25c
Vaal34 <5619@holbertonstudents.com>
2024-02-22 17:44:54 +0100

    return the User ID based on the cookie _my_session_id

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -31,3 +31,13 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ returns a User instance based on a cookie value """
+        if request is None:
+            return None
+
+        session_cookie = self.session_cookie(request)
+
+        user_id = self.user_id_for_session_id(session_cookie)
+        return User.get(user_id)
