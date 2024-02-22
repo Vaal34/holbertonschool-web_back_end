3e3907b004f87e19e9d1b4e861e008982e992de1
Vaal34 <5619@holbertonstudents.com>
2024-02-22 16:39:18 +0100

    Update the @app.before_request

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -59,6 +59,10 @@ def before_request():
                     '/api/v1/forbidden/',
                     '/api/v1/auth_session/login/']
 
+    if auth.authorization_header(request) is None \
+            and auth.session_cookie(request) is None:
+        abort(401)
+        
     if auth.require_auth(request.path, exclude_path) is not True:
         return
 
@@ -68,9 +72,6 @@ def before_request():
     if auth.current_user(request) is None:
         return abort(403)
 
-    if auth.authorization_header(request) is None \
-            and auth.session_cookie(request) is None:
-        abort(401)
 
     request.current_user = auth.current_user(request)
 
