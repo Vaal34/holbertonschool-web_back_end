f33be9e1138de868b7186b562a9393c72fe25d3b
Vaal34 <5619@holbertonstudents.com>
2024-02-22 15:58:09 +0100

    function that returns a User ID based on a Session ID

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -21,14 +21,13 @@ class SessionAuth(Auth):
         if type(user_id) is not str:
             return None
         session_id = uuid.uuid4()
-        self.user_id_by_session_id[session_id] = user_id
-        return session_id
-    
+        self.user_id_by_session_id[str(session_id)] = user_id
+        return str(session_id)
+
     def user_id_for_session_id(self, session_id: str = None) -> str:
         """ that returns a User ID based on a Session ID """
         if session_id is None:
             return None
         if type(session_id) is not str:
             return None
-
         return self.user_id_by_session_id.get(session_id)
