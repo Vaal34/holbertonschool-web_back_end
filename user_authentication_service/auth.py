f95e2e0f50a4386e7949dc3b03812262a1fb79bb
Vaal34 <5619@holbertonstudents.com>
2024-02-27 13:35:00 +0100

    pep8

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -4,6 +4,7 @@ AUTH file
 """
 import bcrypt
 
+
 def _hash_password(password: str) -> bytes:
     """ hash password for security """
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
