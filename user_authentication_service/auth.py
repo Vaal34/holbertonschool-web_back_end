23c97502ba3083cadee3a3c4472ec3bf1d0f67b3
Vaal34 <5619@holbertonstudents.com>
2024-02-27 13:33:19 +0100

    hash password

---
+++ user_authentication_service/auth.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+AUTH file
+"""
+import bcrypt
+
+def _hash_password(password: str) -> bytes:
+    """ hash password for security """
+    return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
