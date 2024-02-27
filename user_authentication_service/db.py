17e8fabdf45d5b011bc622e396c5f37c1b486ec5
Vaal34 <5619@holbertonstudents.com>
2024-02-27 12:08:48 +0100

    add method that find_user

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,7 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
-from sqlalchemy.exc import NoResultFound, InvalidRequestError
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base
 from user import User
@@ -45,7 +46,7 @@ class DB:
         """
         if not kwargs:
             raise InvalidRequestError
-        user = self._session.query(User).filter_by(**kwargs).first()
-        if user is None:
+        user_find = self._session.query(User).filter_by(**kwargs).first()
+        if user_find is None:
             raise NoResultFound
-        return user
+        return user_find
