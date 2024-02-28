111fb00838f933324f82023803865d923fd62f0c
Vaal34 <5619@holbertonstudents.com>
2024-02-28 14:41:39 +0100

    logout route

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -50,12 +50,11 @@ def login():
 def logout():
     """ logout """
     session_id = request.cookies.get("session_id")
-    try:
-        user = AUTH.get_user_from_session_id(session_id)
+    user = AUTH.get_user_from_session_id(session_id)
+    if user:
         AUTH.destroy_session(user.id)
-        return redirect("/", code=200)
-    except NoResultFound:
-        return abort(403)
+        return redirect("/")
+    return abort(403)
 
 
 if __name__ == "__main__":
