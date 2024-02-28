f3d874bbebb6be8e655503c512c070ba61f9c7cd
Vaal34 <5619@holbertonstudents.com>
2024-02-28 15:24:58 +0100

    flask route profile with session_id

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -60,7 +60,7 @@ def logout():
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile():
     """ get profile """
-    cookie_session_id = request.form["session_id"]
+    cookie_session_id = request.cookies.get("session_id")
     user = AUTH.get_user_from_session_id(cookie_session_id)
     if user:
         return jsonify({"email": user.email}), 200
