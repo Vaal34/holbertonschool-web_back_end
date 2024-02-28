74edb56b44e388be8269bf88787313ce1d3dc880
Vaal34 <5619@holbertonstudents.com>
2024-02-28 11:30:33 +0100

    create route register

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -4,8 +4,11 @@ Flask APP
 """
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
+from auth import Auth
+
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/")
@@ -14,5 +17,16 @@ def get_request() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+@app.route("/users", methods=["POST"], strict_slashes=False)
+def register_user():
+    """ register method """
+    try:
+        AUTH.register_user(request.form["email"], request.form["password"])
+        return jsonify({"email": request.form['email'],
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"})
+
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
+    app.run(host="0.0.0.0", port="5000", debug=True)
