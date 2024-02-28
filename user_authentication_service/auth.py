955fd4323489a40b6641146f5f32404af0f10c18
Vaal34 <5619@holbertonstudents.com>
2024-02-28 12:07:39 +0100

    add session id to user

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -48,3 +48,13 @@ class Auth:
                 return False
         except NoResultFound:
             return False
+
+    def create_session(self, email) -> str:
+        """ create session """
+        try:
+            user = self._db.find_user_by(email=email)
+            user.session_id = _generate_uuid()
+            self._db._session.commit()
+            return user.session_id
+        except NoResultFound:
+            return None
