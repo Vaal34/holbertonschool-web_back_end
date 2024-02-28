085df50350c41c1326fde30c2fd4faa9ab8e04c3
Vaal34 <5619@holbertonstudents.com>
2024-02-28 11:50:29 +0100

    method valid_login

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -35,10 +35,7 @@ class Auth:
         """ check is valid login """
         try:
             user = self._db.find_user_by(email=email)
-            if bcrypt.checkpw(password.encode("utf-8"),
-                              user.hashed_password):
-                return True
-            else:
-                return False
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
         except NoResultFound:
             return False
