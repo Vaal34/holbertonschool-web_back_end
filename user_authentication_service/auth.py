ef172252e4dd7b3fcc07544a232f9438faba06a3
Vaal34 <5619@holbertonstudents.com>
2024-02-28 12:00:57 +0100

    generate uuid

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -15,6 +15,11 @@ def _hash_password(password: str) -> bytes:
     return bcrypt.hashpw(password.encode("UTF-8"), bcrypt.gensalt())
 
 
+def _generate_uuid() -> str:
+    """ generate uuid """
+    return str(uuid.uuid4())
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
@@ -43,7 +48,3 @@ class Auth:
                 return False
         except NoResultFound:
             return False
-
-    def _generate_uuid() -> str:
-        """ generate uuid """
-        return str(uuid.uuid4())
