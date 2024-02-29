a68eda96a15cd2bf9f3e03a317b4dd38c8103d56
Vaal34 <5619@holbertonstudents.com>
2024-02-29 13:05:35 +0100

    ROUTE THAT UPDATE PASSWORD

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -86,12 +86,11 @@ def update_password():
     reset_token = request.form.get('reset_token')
     new_password = request.form.get('new_password')
     try:
-        AUTH.update_password(reset_token=reset_token,
-                             new_password=new_password)
+        AUTH.update_password(reset_token, new_password)
         return jsonify({"email": email, "message": "Password updated"}), 200
     except ValueError:
         abort(403)
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000", debug=True)
+    app.run(host="0.0.0.0", port="5000")
