f723a0d8e864aaa9a05af041de72d0046dfd7d4c
Vaal34 <5619@holbertonstudents.com>
2024-03-06 11:52:27 +0100

    annotation

--- i18n/2-app.py
+++ i18n/2-app.py
@@ -13,15 +13,15 @@ class Config():
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
-
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
 
 @babel.localeselector
-def get_locale():
+def get_locale() -> str:
     """ get local languages """
     return request.accept_languages.best_match(Config.LANGUAGES)
+
 
 @app.route("/")
 def index():
