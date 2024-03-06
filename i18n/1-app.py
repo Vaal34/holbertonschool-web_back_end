fd06d9a724381de4a62372e4eebebcbcff4b5263
Vaal34 <5619@holbertonstudents.com>
2024-03-06 11:25:50 +0100

    config babel

---
+++ i18n/1-app.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""
+flask app
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config():
+    """ class Config """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route("/")
+def index():
+    """ index """
+    return render_template("1-index.html")
