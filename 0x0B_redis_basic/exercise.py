bd93d0a6bcd6c41131e62a05e73ef5a40f27409f
Vaal34 <5619@holbertonstudents.com>
2024-03-19 15:06:01 +0100

    annoatate

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -38,7 +38,8 @@ class Cache():
         self._redis.set(random_key, data)
         return random_key
 
-    def get(self, key: str, fn: Optional[Callable] = None):
+    def get(self, key: str,
+            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
         """ Store input data in Redis using a
         random key and return the key """
         data = self._redis.get(key)
