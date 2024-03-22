78f47077c9b07ef9a358f3759409a5fb7eff9bb9
Vaal34 <5619@holbertonstudents.com>
2024-03-22 11:33:26 +0100

    add collumn country

--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE IF NOT EXISTS users (
     -- Colonne "email" de type VARCHAR, ne pouvant pas être vide (NOT NULL) et devant être unique
     email VARCHAR(255) NOT NULL UNIQUE,
     -- Colonne "name" de type VARCHAR, pouvant contenir le nom de l'utilisateur
-    name VARCHAR(255)
+    name VARCHAR(255),
     -- Colonne "country" de type ENUM avec les valeurs 'US' 'CO' 'TN' par DEFAULT 'US'
     country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
 );
