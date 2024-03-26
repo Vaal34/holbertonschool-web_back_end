c37d8f6274d7a8d00b3120883999a3ca266f5fa5
Vaal34 <5619@holbertonstudents.com>
2024-03-26 18:57:06 +0100

    add argument type for choice a divide, sum or subtract

---
+++ unittests_in_js/1-calcul.test.js
@@ -0,0 +1,19 @@
+var assert = require("assert");
+const calculateNumber = require('./1-calcul.js');
+
+describe('Test Rounded', function(){
+    it('should work when type is SUM', function(){
+        assert.equal(calculateNumber("SUM", 2, 3), 5);
+        assert.equal(calculateNumber("SUM", -2.56, -17,89), -20);
+    });
+    it('should work when type is SUBTRACT', function(){
+        assert.equal(calculateNumber("SUBTRACT", 2, 3), -1)
+        assert.equal(calculateNumber("SUBTRACT", 5.75, 3.25), 3)
+        assert.equal(calculateNumber("SUBTRACT", -10.25, -11), 1)
+    });
+    it('should work when type is DIVIDE', function(){
+        assert.equal(calculateNumber("DIVIDE", 4.25, 2.25), 2)
+        assert.equal(calculateNumber("DIVIDE", 81.23, 8.75), 9)
+        assert.equal(calculateNumber("DIVIDE", -10.25, 0), "Error")
+    });
+})
