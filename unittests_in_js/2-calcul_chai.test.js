c6872407f56ab2b7b7081c578de71f03325d744d
Vaal34 <5619@holbertonstudents.com>
2024-03-27 11:44:38 +0100

    test

--- unittests_in_js/2-calcul_chai.test.js
+++ unittests_in_js/2-calcul_chai.test.js
@@ -1,4 +1,4 @@
-const expect = require("chai").expect();
+const expect = require('chai').expect();
 const calculateNumber = require('./2-calcul_chai.js');
 
 describe('Test Calcul function', function(){
