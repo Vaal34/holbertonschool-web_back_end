3e590f7f63e50bd8c6a2e3952b07d0942e2a2070
Vaal34 <5619@holbertonstudents.com>
2024-03-28 14:34:00 +0100

    test with args in api request

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -5,6 +5,7 @@ describe("Index page", function(){
     it("should the correct status code 200 when id is a number", function(done){
         request("http://localhost:7865/cart/12", function(err, response, body){
             assert.equal(response.statusCode, 200)
+            assert.equal(response.body, "Payment methods for cart 12")
             done();
         })
     });
