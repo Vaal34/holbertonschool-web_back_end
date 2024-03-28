8873cffe232712bb31485e0456ce1ac827537c66
Vaal34 <5619@holbertonstudents.com>
2024-03-28 14:40:03 +0100

    test with args in api request

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -3,14 +3,14 @@ const request = require("request")
 
 describe("Index page", function(){
     it("should the correct status code 200 when id is a number", function(done){
-        request("http://localhost:7865/cart/12", function(err, response, body){
+        request.get("http://localhost:7865/cart/12", function(err, response, body){
             assert.equal(response.statusCode, 200)
             assert.equal(response.body, "Payment methods for cart 12")
             done();
         })
     });
     it("should the correct status code 200 when id is not a number", function(done){
-        request("http://localhost:7865/cart/ze", function(err, response, body){
+        request.get("http://localhost:7865/cart/test", function(err, response, body){
             assert.equal(response.statusCode, 404)
             done();
         })
