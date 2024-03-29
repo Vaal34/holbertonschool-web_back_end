064eed7e7e5290046a3e47d15a0775a9ab82709c
Vaal34 <5619@holbertonstudents.com>
2024-03-29 19:53:40 +0100

    add test get /

--- unittests_in_js/9-api/api.js
+++ unittests_in_js/9-api/api.js
@@ -2,6 +2,10 @@ const express = require("express")
 const app = express();
 const port = 7865;
 
+app.get('/', (request, response) => {
+    response.send('Welcome to the payment system');
+});
+
 app.get("/cart/:id([0-9]+)", function(request, response){
     const id = request.params.id
     response.send(`Payment methods for cart ${id}`)
--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -1,16 +1,29 @@
-const assert = require("assert")
+const assert = require("assert");
+const { response } = require("express");
 const request = require("request")
 
 describe("Index page", function(){
     it("should the correct status code 200 when id is a number", function(done){
-        request.get("http://localhost:7865/cart/12", function(err, response, body){
+        options = {
+            url: 'http://localhost:7865',
+            method: 'GET',
+        }
+        request("http://localhost:7865/cart/12", function(err, response, body){
             assert.equal(response.statusCode, 200)
             assert.equal(response.body, "Payment methods for cart 12")
             done();
         })
     });
+
+    it("should return SC 200 and Welcome to the payment system", function(done){
+        request.get("http://localhost:7865/", function(e, r, b){
+            assert.equal(r.statusCode, 200)
+            assert.equal(r.body, "Welcome to the payment system")
+        })
+    })
+
     it("should the correct status code 200 when id is not a number", function(done){
-        request.get("http://localhost:7865/cart/test", function(err, response, body){
+        request("http://localhost:7865/cart/test", function(err, response, body){
             assert.equal(response.statusCode, 404)
             done();
         })
